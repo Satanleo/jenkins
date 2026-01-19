cb301657f4981bdc1ddb983b04a4c505dc704c9d
Saket Wathore <66241810+Satanleo@users.noreply.github.com>
2026-01-20 00:25:55 +0530

    python hello world

--- first.sh
+++ first.sh
@@ -1,12 +1,1 @@
-!/bin/bash
-echo "hello buddy from git"
-echo "second line added"
-echo "third line added"
-echo "fourth line added"
-echo "fifth line added"
-echo sixth line added"
-echo "seventh line added"
-
-
-
-
+print("Hello, World")
